2446a69b8b45719a6dd5c1614051316f1f6c13e1
Danika Thomson <danikathomson@Danikas-mac.local>
2020-01-31 00:09:47 +0100

    retrieved Rick and Morty characters with Axios App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import axios from "axios";
 import Header from "./components/Header.js";
 
 
 export default function App() {
+  const [characters, setCharacters] = useState([]);
+  
+  useEffect(() => {
+    axios.get("https://rickandmortyapi.com/api/character/")
+    .then(response => {
+       setCharacters(response.data.results);
+      console.log(response.data);
+    })
+
+  .catch(error => {
+    console.log("Uh ohhh! Where's my data??!!", error);
+});
+
+}, []);
+  
+  
+  
   return (
     <main>
       <Header />
     </main>
   );
 }
+//https://rickandmortyapi.com/api/character/`}
